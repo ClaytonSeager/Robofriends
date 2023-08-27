396adc7eb65463688fb257196d56eec771ea4cd2
ClaytonSeager <seager.clay@gmail.com>
2023-08-26 23:14:48 -0400

    Uploading from Webstorm via Git

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends React.Component {
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learning React
+            Learning React!
           </a>
         </header>
       </div>
